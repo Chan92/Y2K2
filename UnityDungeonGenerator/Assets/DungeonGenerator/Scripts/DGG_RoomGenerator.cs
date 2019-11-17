57e8a63b8ce3e37af03bd9f19eb95e1ba15505a2
chan1992 <c_voort1992@hotmail.com>
2019-11-17 23:25:28 +0100

    Experimenting codes

---
+++ UnityDungeonGenerator/Assets/DungeonGenerator/Scripts/DGG_RoomGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+[System.Serializable]
+public class ObjectList {
+	public int objectAmount;
+	public Transform objectPrefab;
+}
+
+public class DGG_RoomGenerator : MonoBehaviour {
+	[SerializeField]
+	private string roomNames = "Room";
+	[SerializeField]
+	private int roomAmount = 1;
+	[SerializeField]
+	private int roomWidth = 1, roomLength = 1, roomHeight = 1;
+	[SerializeField][Range(1, 4)]
+	private int doorAmount = 1;
+	[SerializeField]
+	private RoomType roomType;
+	[SerializeField]
+	private Material groundMaterial, wallMaterial, ceilingMaterial;
+	[SerializeField]
+	private GameObject baseRoom;
+	[SerializeField]
+	private Transform doorPrefab;
+	[SerializeField]
+	private ObjectList[] objects;
+	[Space(25)]
+	[SerializeField]
+	private string savePath = "Assets/DungeonGenerator/Prefabs/Rooms/";
+
+	public void Generate(){
+		int _nameCounter = 1;
+
+		for(int i = 0; i < roomAmount; i++) {
+			string _newName = roomNames + _nameCounter.ToString();
+			while(File.Exists(savePath + _newName)) {
+				_nameCounter++;
+				_newName = roomNames + _nameCounter.ToString();
+			}
+
+			//GameObject _newRoom = Instantiate(baseRoom);
+			GameObject _newRoom = (GameObject) PrefabUtility.InstantiatePrefab(baseRoom);
+			_newRoom.name = _newName;
+			PrefabUtility.SaveAsPrefabAsset(_newRoom, savePath);
+		}
+    }
+
+}
---
+++ UnityDungeonGenerator/Assets/DungeonGenerator/Scripts/Editor/DGG_RoomGeneratorEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DGG_RoomGenerator))]
+public class DGG_RoomGeneratorEditor: Editor{
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		DGG_RoomGenerator myRGScript = (DGG_RoomGenerator) target;
+		if(GUILayout.Button("Build Object")) {
+			myRGScript.Generate();
+		}
+	}
+}
