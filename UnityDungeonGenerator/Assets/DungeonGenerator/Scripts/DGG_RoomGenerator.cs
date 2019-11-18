a1a8bd5dc7f7881f3db1eb699892219f3307cd6a
Chantal <c_voort1992@hotmail.com>
2019-11-18 14:46:28 +0100

    Small update to RoomGenerator

--- UnityDungeonGenerator/Assets/DungeonGenerator/Scripts/DGG_RoomGenerator.cs
+++ UnityDungeonGenerator/Assets/DungeonGenerator/Scripts/DGG_RoomGenerator.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.IO;
+using UnityEngine;
 using UnityEditor;
 
 [System.Serializable]
@@ -34,24 +34,51 @@ public class DGG_RoomGenerator : MonoBehaviour {
 	private string savePath = "DungeonGenerator/Prefabs/Rooms/";
 
 	public void Generate(){
+		//start with 1 as non-developers arent used to start counting by 0
+		//restart with 1 each time u generate to fill deleted slots
 		int _nameCounter = 1;
 
+		//generates the amount of rooms requested
 		for(int i = 0; i < roomAmount; i++) {
 			string _newName = roomNames + _nameCounter.ToString();
 
+			//if an object with the name already exist, update the counter
 			while(File.Exists("Assets/" + savePath + _newName + ".prefab")) {
 				_nameCounter++;
 				_newName = roomNames + _nameCounter.ToString();
-				print("new name");
 			}
 
 			GameObject _newRoom = (GameObject) PrefabUtility.InstantiatePrefab(baseRoom);
-			_newRoom.name = _newName;
+			_newRoom.transform.localScale = new Vector3(roomWidth, roomHeight, roomLength);
+			AddRoomDoors(_newRoom.transform);
+			FillRoom(_newRoom.transform);
 
+			//save the room
 			string path = Path.Combine(Application.dataPath, savePath);
 			path.Replace("\\", "/");
-			PrefabUtility.SaveAsPrefabAsset(_newRoom, path + _newName +".prefab");
+			PrefabUtility.SaveAsPrefabAsset(_newRoom, path + _newName + ".prefab");
+			DestroyImmediate(_newRoom, false);
 		}
     }
+
+	private void AddRoomDoors(Transform _room) {
+
+	}
+
+	//add objects to the room
+	private void FillRoom(Transform _room) {
+		if (objects.Length < 1) {
+			return;
+		}
+		
+		for (int i = 0; i < objects.Length; i++) {
+			if (objects[i].objectAmount >= 1) {
+				for(int _object = 0; _object < objects[i].objectAmount; _object++) {
+					Transform _newObject = Instantiate(objects[i].objectPrefab);
+					_newObject.parent = _room;
+				}
+			}
+		}
+	}
 
 }
