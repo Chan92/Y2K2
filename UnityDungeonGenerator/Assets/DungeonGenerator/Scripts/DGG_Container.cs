596245a2cc485e80b1444f261634638271a27ff4
Chantal <c_voort1992@hotmail.com>
2019-11-27 14:00:55 +0100

    added variables container

---
+++ UnityDungeonGenerator/Assets/DungeonGenerator/Scripts/DGG_Container.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+namespace Funtools.DungeonGenerator {
+	[System.Serializable]
+	public struct ObjectList {
+		public int objectAmount;
+		public Transform objectPrefab;
+	}
+
+	public class RoomInfo :ScriptableObject {
+		static RoomInfo instance;
+		public static RoomInfo Instance {
+			get {
+				if(instance == null) {
+					instance = new RoomInfo();
+				}
+				return instance;
+			}
+		}
+
+		public string roomNames = "Room";
+		public int roomWidth = 1, roomLength = 1, roomHeight = 1;
+		public int roomAmount = 1, doorAmount = 1;
+
+		public Material groundMaterial, wallMaterial, ceilingMaterial;
+		public Transform baseRoom, doorPrefab;
+		public ReorderableList myList;
+		public SerializedObject serializedObject;
+		public bool isInitialized = false;
+
+		[SerializeField]
+		public List<ObjectList> objects;
+		public RoomType roomType;
+
+		public string savePath = "DungeonGenerator/Prefabs/Rooms/";
+		public const int minDoors = 1, maxDoors = 4;
+
+		private void OnEnable() {
+			serializedObject = new SerializedObject(this);
+		}
+	}
+}
